00d226b77b36ba95435c72cedb0a4bc6a0126b2e
AbhiKarma1 <124451257+abhishikkarmakar@users.noreply.github.com>
2023-11-15 19:46:54 +0530

    Update handTracker.py

--- handTracker.py
+++ handTracker.py
@@ -2,14 +2,15 @@ import mediapipe as mp
 import numpy as np
 import cv2
 
-
 class HandTracker():
     def __init__(self, mode=False, model_complexity=1, maxHands=2, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+        # Initialize HandTracker with given parameters
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
+        # Define MediaPipe hands module and initialize hand tracking
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
@@ -19,9 +20,12 @@ class HandTracker():
         self.mp_draw = mp.solutions.drawing_utils
 
     def findHands(self, img, draw=True):
+        # Convert image to RGB format
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Process hands in the image
         self.results = self.hands.process(imgRGB)
 
+        # If hand landmarks are found, draw them on the image
         if self.results.multi_hand_landmarks:
             for handLm in self.results.multi_hand_landmarks:
                 if draw:
@@ -29,6 +33,7 @@ class HandTracker():
         return img
 
     def getPostion(self, img, handNo=0, draw=True):
+        # Retrieve landmarks for a specific hand from the processed results
         lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -42,9 +47,11 @@ class HandTracker():
         return lmList
 
     def getUpFingers(self, img):
+        # Determine fingers that are up based on landmark positions
         pos = self.getPostion(img, draw=False)
         self.upfingers = []
         if pos:
+            # Check each finger's position and append True/False for each
             # thumb
             self.upfingers.append((pos[4][1] < pos[3][1] and (pos[5][0] - pos[4][0] > 10)))
             # index
